82c76e8ddd5339d3e08e3f6c15d83acc92a0ed79
NewUserName <ramanpal7228@gmail.com>
2025-08-06 11:52:58 +0530

    updated

--- first_Project/firstapp/urls.py
+++ first_Project/firstapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from firstapp import views
 
 urlpatterns = [
-    path("home", views.hello),
+    path("",views.hello),
     path("check", views.findEvenOdd),
     
     path("getData/<int:id>/", views.getData),
