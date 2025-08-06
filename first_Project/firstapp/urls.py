154764a19a709bc228f786bf437500a2b712e25a
NewUserName <ramanpal7228@gmail.com>
2025-08-06 10:36:52 +0530

    fetching api added

--- first_Project/firstapp/urls.py
+++ first_Project/firstapp/urls.py
@@ -4,4 +4,7 @@ from firstapp import views
 urlpatterns = [
     path("home", views.hello),
     path("check", views.findEvenOdd),
+    
+    path("getData/<int:id>/", views.getData),
+    
 ]
--- first_Project/firstapp/views.py
+++ first_Project/firstapp/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -15,3 +17,12 @@ def findEvenOdd(request):
     else:
         return HttpResponse("odd number")
 
+
+def getData(request, id):
+    url = f"https://jsonplaceholder.typicode.com/posts/{id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return JsonResponse(data)
+    else:
+        return JsonResponse({"error": "Data not found"}, status=404)
